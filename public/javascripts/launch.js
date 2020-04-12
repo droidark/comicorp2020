45fdcf105e3946f5366b370f5722802df87a9322
droidark <droidark@outlook.com>
2020-04-11 22:49:40 -0400

    Issue with post color, fixed?

--- public/javascripts/launch.js
+++ public/javascripts/launch.js
@@ -191,8 +191,10 @@ const build = {
             .append($.link);
         $.cardImgOverlay = $('<div>', {class: 'card-img-overlay'})
             .append($.cardText)
-            .append($.cardTitle)
-            .append(buildScore.simple(post.score.average, 'card-text', post.color));
+            .append($.cardTitle);
+            if(post.score != null) {
+                $.cardImgOverlay.append(buildScore.simple(post.score.average, 'card-text', post.color));
+            }
         $.cardImg = $('<img>', {class: 'card-img', src: post.thumb, alt: post.title});
         $.card = $('<div>', {class: 'card rounded-0'})
             .append($.cardImg)

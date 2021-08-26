13b6f24d36eea754076da6a32e7acfce50240a8d
droidark <droidark@outlook.com>
2021-08-25 19:31:49 -0500

    Changes on js files

--- public/javascripts/launch.js
+++ public/javascripts/launch.js
@@ -326,14 +326,14 @@ const darkMode = {
         $('#dark-mode').prop('checked', isDark);
         if(isDark) {
             $('body').addClass('dark-mode');
-            $('.sans-serif').addClass('dark');
+            $('#disqus_thread .sans-serif').addClass('dark');
             $('.navbar').removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark');
             $('#dark-mode + label').addClass('text-light');
             $('.sidebar .title-wrapper, #Blog1 .title-wrapper').addClass('title-wrapper-dunkel');
             $('#previousPage, #nextPage').addClass('btn btn-dark active');
         } else {
             $('body').removeClass('dark-mode');
-            $('.sans-serif').removeClass('dark');
+            $('disqus_thread .sans-serif').removeClass('dark');
             $('.navbar').removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light');
             $('#dark-mode + label').removeClass('text-light');
             $('.sidebar .title-wrapper, #Blog1 .title-wrapper').removeClass('title-wrapper-dunkel');
